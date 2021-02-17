2aa64dd87d81189eabac2f71af42959bbfeacfb6
Jonas Buntinx <buntinxjonas@gmail.com>
2021-02-17 18:57:18 +0900

    Added interaction

--- src/Components/Map.tsx
+++ src/Components/Map.tsx
@@ -1,7 +1,12 @@
 import * as MapboxGL from "mapbox-gl";
 import * as React from "react";
 
-function Map(): JSX.Element {
+type Props = {
+  center: MapboxGL.LngLat;
+  zoom: number;
+};
+
+function Map(props: Props): JSX.Element {
   const ref = React.useRef<HTMLDivElement>(null);
 
   const [mapInstance, setMapInstance] = React.useState<MapboxGL.Map | null>(null);
@@ -14,14 +19,22 @@ function Map(): JSX.Element {
     const map = new MapboxGL.Map({
       container: ref.current,
       style: "mapbox://styles/mapbox/streets-v11",
-      center: [139.68, 35.68],
-      zoom: 9,
+      center: props.center,
+      zoom: props.zoom,
     });
 
     setMapInstance(map);
-  }, [mapInstance, setMapInstance, ref]);
+  }, [mapInstance, setMapInstance, ref, props.center, props.zoom]);
 
-  return <div ref={ref} style={{ width: "640px", height: "480px" }} />;
+  React.useEffect(() => {
+    if (!mapInstance) {
+      return;
+    }
+
+    mapInstance.flyTo({ center: props.center, zoom: props.zoom });
+  }, [mapInstance, props.center, props.zoom]);
+
+  return <div ref={ref} className="w-full h-full" />;
 }
 
 export { Map };
--- src/Components/Root.tsx
+++ src/Components/Root.tsx
@@ -1,8 +1,62 @@
 import * as React from "react";
+import * as MapboxGL from "mapbox-gl";
 import { Map } from "~/Components/Map";
 
+type State = {
+  center: MapboxGL.LngLat;
+  zoom: number;
+};
+
+type Actions = { tag: "UpdateCenter"; center: MapboxGL.LngLat } | { tag: "Reset" };
+
 function Root(): JSX.Element {
-  return <Map />;
+  const data = [
+    { name: "Edo Castle", location: new MapboxGL.LngLat(139.752496, 35.6877513) },
+    { name: "Matsumoto Castle", location: new MapboxGL.LngLat(137.9666734, 36.2386573) },
+    { name: "Osaka Castle", location: new MapboxGL.LngLat(135.5237615, 34.6873377) },
+  ];
+
+  const initialState = { center: data[0] ? data[0].location : new MapboxGL.LngLat(0.0, 0.0), zoom: 12.0 };
+
+  const reducer: React.Reducer<State, Actions> = (state: State, action: Actions) => {
+    switch (action.tag) {
+      case "UpdateCenter":
+        return { ...state, center: action.center };
+      case "Reset":
+        return { ...initialState, center: Object.assign({}, initialState.center) };
+      default:
+        throw new Error();
+    }
+  };
+
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
+  return (
+    <div className="h-screen flex">
+      <div className="w-1/4 flex flex-col">
+        {data.map(({ name, location }, key) => (
+          <button
+            key={key}
+            onClick={() => {
+              dispatch({ tag: "UpdateCenter", center: location });
+            }}
+          >
+            {name}
+          </button>
+        ))}
+        <button
+          onClick={() => {
+            dispatch({ tag: "Reset" });
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      <div className="flex-1">
+        <Map center={state.center} zoom={state.zoom} />
+      </div>
+    </div>
+  );
 }
 
 export { Root };
